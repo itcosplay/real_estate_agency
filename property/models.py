ea7077cdc55a723dbb3a77e05f29c832cc86760c
Konstantine Pialov <konstantinepialov@MacBook-Air-Konstantine.local>
2021-12-01 16:54:43 +0300

    Add model field Flat.new_building

--- property/models.py
+++ property/models.py
@@ -39,6 +39,7 @@ class Flat(models.Model):
         blank=True,
         db_index=True)
 
+    new_building = models.NullBooleanField('Новостройка', db_index=True)
     has_balcony = models.NullBooleanField('Наличие балкона', db_index=True)
     active = models.BooleanField('Активно-ли объявление', db_index=True)
     construction_year = models.IntegerField(
