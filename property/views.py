a3e3e2c32809dbb91f0820ac8de5914f9482809a
itcosplay <azure_dream@mail.ru>
2021-12-03 13:35:13 +0300

    Enable filter through new buildings on index page

--- property/views.py
+++ property/views.py
@@ -24,13 +24,23 @@ def show_flats(request):
         flats = flats.filter(price__gt=min_price)
     if max_price:
         flats = flats.filter(price__lt=max_price)
+    if new_building:
+        flats = flats.filter(new_building=True)
 
-    towns = Flat.objects.values_list(
-        'town', flat=True).distinct().order_by('town')
-    return render(request, 'flats_list.html', {
-        'flats': flats[:10],
-        'towns': towns,
-        'active_town': town,
-        'max_price': max_price,
-        'min_price': min_price,
-        'new_building': new_building})
+    towns = Flat.objects.values_list (
+        'town',
+        flat=True
+    ).distinct().order_by('town')
+
+    return render (
+        request,
+        'flats_list.html',
+        {
+            'flats': flats[:10],
+            'towns': towns,
+            'active_town': town,
+            'max_price': max_price,
+            'min_price': min_price,
+            'new_building': new_building
+        }
+    )
